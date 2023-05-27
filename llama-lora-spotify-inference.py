56335836a51fbbef3ac5a567e6c49a170776e665
Amir Kidwai <40836125+cumbersomeamir@users.noreply.github.com>
2023-05-27 22:05:32 +0100

    Update llama-lora-spotify-inference.py

--- llama-lora-spotify-inference.py
+++ llama-lora-spotify-inference.py
@@ -1,8 +1,12 @@
+import os
+
+import bitsandbytes as bnb
 import torch
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-peft_model_id = "Amirkid/llama-lora-spotify" 
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+peft_model_id = "Amirkid/llama-lora-spotify"
 config = PeftConfig.from_pretrained(peft_model_id)
 model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path, return_dict=True, load_in_8bit=True, device_map='auto')
 tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
@@ -10,9 +14,16 @@ tokenizer = AutoTokenizer.from_pretrained(config.base_model_name_or_path)
 # Load the Lora model
 model = PeftModel.from_pretrained(model, peft_model_id)
 
+batch = tokenizer("Amir opened up with this joke", return_tensors='pt')
+
+with torch.cuda.amp.autocast():
+  output_tokens = model.generate(**batch, max_new_tokens=50)
+
+print('\n\n', tokenizer.decode(output_tokens[0], skip_special_tokens=True))
 
 
-batch = tokenizer("Two things are infinite: ", return_tensors='pt')
+
+batch = tokenizer("Amir opened up with this joke", return_tensors='pt')
 
 with torch.cuda.amp.autocast():
   output_tokens = model.generate(**batch, max_new_tokens=50)
